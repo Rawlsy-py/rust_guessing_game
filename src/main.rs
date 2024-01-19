a8fe633e86462abef5320c085791584b9ce96784
Rawlsy-py <christopher_m_mitchell@live.co.uk>
2024-01-19 20:25:41 +0000

    loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use rand::Rng;
-use std::io;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Welcome to Rust Guessing Game!");
@@ -9,22 +9,26 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please enter your guess as a number.");
+    loop {
+        println!("Please enter your guess as a number.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse()
-            .expect("Please type a number!");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small"),
-        Ordering::Equal => println!("Correct"),
-        Ordering::Greater => println!("Too Big"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Equal => println!("Correct"),
+            Ordering::Greater => {
+                println!("Too Big");
+                break;
+            }
+        }
     }
 }
